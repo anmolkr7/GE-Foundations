3f7914670c5699cc6d070c19d705b9f8eeb331d0
Anmol Kumar Ojha <150778576+anmolkr7@users.noreply.github.com>
2025-12-05 12:38:16 +0530

    Change output message to 'Hello WORLD'

--- src/Hello.java
+++ src/Hello.java
@@ -1,8 +1,9 @@
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Hello world. Let's begin to CODE!");
+        System.out.println("Hello WORLD. Let's begin to CODE!");
     }
 }
+
 
 
 
