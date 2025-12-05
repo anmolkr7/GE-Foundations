1139ff407ca18d418d998273f9210bd7ec8d53eb
anmolkr7 <anmolojha952@gmail.com>
2025-12-05 11:50:59 +0530

    local change

--- src/Hello.java
+++ src/Hello.java
@@ -1,6 +1,6 @@
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Hello World. Let's begin!");
+        System.out.println("Hello WOrld. Let's begin!");
     }
 }
 
