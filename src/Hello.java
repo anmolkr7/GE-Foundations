ba4446b451c5ae7cb3f9c7a7c487c7b3c43d1e2d
Anmol Kumar Ojha <150778576+anmolkr7@users.noreply.github.com>
2025-12-05 12:30:42 +0530

    Update greeting message

--- src/Hello.java
+++ src/Hello.java
@@ -1,7 +1,8 @@
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Hello world. Let's begin!");
+        System.out.println("Hello world. Let's begin to code!");
     }
 }
+
 
 
