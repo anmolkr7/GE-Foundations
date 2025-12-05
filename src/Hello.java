4c9c59c80e54f140f70ceb933763e44a06bb1841
anmolkr7 <anmolojha952@gmail.com>
2025-12-05 12:34:03 +0530

    creating branch conflicts

--- src/Hello.java
+++ src/Hello.java
@@ -1,6 +1,6 @@
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Hello world. Let's begin to code!");
+        System.out.println("Hello world. Let's begin to CODE!");
     }
 }
 
