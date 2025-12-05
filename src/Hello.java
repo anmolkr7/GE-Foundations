1214ce081ac7215367e424fd254a54765ce6be6c
anmolkr7 <anmolojha952@gmail.com>
2025-12-05 12:39:47 +0530

    local branch change

--- src/Hello.java
+++ src/Hello.java
@@ -1,6 +1,6 @@
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Hello world. Let's begin to CODE!");
+        System.out.println("Hello world. Let's BEGIN to CODE!");
     }
 }
 
