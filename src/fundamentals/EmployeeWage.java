e195de9b7bec352c8e696226818a5c43568e7c51
anmolkr7 <anmolojha952@gmail.com>
2025-12-07 13:02:58 +0530

    feat: add employee attendance check logic using random

--- src/fundamentals/EmployeeWage.java
+++ src/fundamentals/EmployeeWage.java
@@ -3,5 +3,14 @@ package fundamentals;
 public class EmployeeWage {
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage Computation Program on Master Branch");
+        //Constants
+        int IS_FULL_TIME=1;
+        //Generates 0,1 to replicate present or absent
+        int empCheck=(int)(Math.floor(Math.random()*10)%2);
+        //Applying condition check to mark attendance
+        if(empCheck==IS_FULL_TIME)
+            System.out.println("Employee is Present");
+        else
+            System.out.println("Employee is Absent");
     }
 }
