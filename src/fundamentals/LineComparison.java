0d62202d1f5ccd96b7d6245c599868cc175d2549
anmolkr7 <anmolojha952@gmail.com>
2025-12-07 13:50:27 +0530

    feat(UC1): calculate line length from two points using distance formula

--- src/fundamentals/LineComparison.java
+++ src/fundamentals/LineComparison.java
@@ -1,7 +1,30 @@
 package fundamentals;
+import java.util.Scanner;
 
 public class LineComparison {
     public static void main(String[] args){
         System.out.println("Welcome to Line Comparison Computation Program on Master Branch");
+
+        // Scanner for user input
+        Scanner sc=new Scanner(System.in);
+
+        // All coordinate variables
+        int x1, y1, x2, y2;
+
+        // Taking first point input
+        System.out.println("Enter x1 y1:");
+        x1=sc.nextInt();
+        y1=sc.nextInt();
+
+        // Taking second point input
+        System.out.println("Enter x2 y2:");
+        x2=sc.nextInt();
+        y2=sc.nextInt();
+
+        // Calculating distance using formula
+        double length=Math.sqrt((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1));
+
+        // Displaying the final computed length
+        System.out.println("Length of the Line="+length);
     }
 }
