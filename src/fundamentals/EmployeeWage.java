f5b4296b52f9ce72c7f498c8feb714ceb7a0d29d
anmolkr7 <anmolojha952@gmail.com>
2025-12-07 13:15:47 +0530

    feat: add part-time employee support and wage calculation

--- src/fundamentals/EmployeeWage.java
+++ src/fundamentals/EmployeeWage.java
@@ -5,22 +5,29 @@ public class EmployeeWage {
         System.out.println("Welcome to Employee Wage Computation Program on Master Branch");
         //Constants
         int IS_FULL_TIME=1;
+        int IS_PART_TIME=2;
         int WAGE_PER_HOUR=20;
         int FULL_DAY_HOURS=8;
+        int PART_TIME_HOURS=8;
 
-        //Generates 0,1 to replicate present or absent
-        int empCheck=(int)(Math.floor(Math.random()*10)%2);
+        //Generates 0,1,2 to replicate absent,present,part-time
+        int empCheck=(int)(Math.floor(Math.random()*10)%3);
+
+        int empHours = 0;
 
         //Applying condition check to mark attendance
         if (empCheck==IS_FULL_TIME) {
-            System.out.println("Employee is Present");
-            // Calculating daily wage
-            int dailyWage=WAGE_PER_HOUR*FULL_DAY_HOURS;
-            System.out.println("Daily Employee Wage: "+dailyWage);
-
-        } else {
-            System.out.println("Employee is Absent");
-            System.out.println("Daily Employee Wage: 0");
+            System.out.println("Employee is Present Full Time");
+            empHours=FULL_DAY_HOURS;
+        } else if(empCheck==IS_PART_TIME){
+            System.out.println("Employee is Present Part-time");
+            empHours=PART_TIME_HOURS;
         }
+        else{
+            System.out.println("Employee is Absent");
+        }
+        // Calculating daily wage
+        int dailyWage=WAGE_PER_HOUR*empHours;
+        System.out.println("Daily Employee Wage: "+dailyWage);
     }
 }
