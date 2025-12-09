2612d0e6bacb92e9dfca17dadb63ffcffed53ac4
anmolkr7 <anmolojha952@gmail.com>
2025-12-09 13:10:04 +0530

    UC7: Added two player simulation with move on ladder

--- src/snakeandladder/Game.java
+++ src/snakeandladder/Game.java
@@ -1,26 +1,33 @@
 package snakeandladder;
 
 public class Game {
-    //single player instance for current game
-    private Player player;
+    //Two player instance for current game
+    private Player player1;
+    private Player player2;
+
     //Dice roll
     private Dice dice;
 
     //Initializes the game with a default player
     public Game(){
-        player=new Player();
+        player1=new Player("Player 1");
+        player2=new Player("Player 2");
         dice=new Dice();
     }
 
     //Starts the game
     public void playGame(){
         System.out.println("Welcome to Snake and Ladder Game");
-        System.out.println("Player starting position:"+player.getPosition());
+
 
         int diceCount=0;
 
+        Player current=player1; //Player 1 starts the game
+
         //Loop until position=100
-        while(player.getPosition()<100) {
+        while(player1.getPosition()<100 && player2.getPosition()<100){
+            System.out.println("\n"+current.getName()+"'s turn:");
+
             //Roll the dice
             int diceValue = dice.roll();
             diceCount++;
@@ -28,14 +35,18 @@ public class Game {
 
             //Generates 0,1,2 corresponding to No Play, Ladder, Snake
             int option = (int) (Math.floor(Math.random() * 10) % 3);
-            int currentPosition = player.getPosition();
+            int currentPosition = current.getPosition();
+            boolean gotLadder=false;
 
             if (option == 0) {
                 System.out.println("No Play"); //position remains same
-            } else if (option == 1) {
+            }
+            else if (option == 1) {
                 System.out.println("Ladder");
                 currentPosition += diceValue; //move forward
-            } else {
+                gotLadder=true;
+            }
+            else {
                 System.out.println("Snake");
                 currentPosition -= diceValue; //move backwards
             }
@@ -48,13 +59,28 @@ public class Game {
             //Exact 100 winning rule
             if(currentPosition>100){
                 System.out.println("Move exceeds 100, stay at same position");
-                currentPosition=player.getPosition(); //ignore move
+                currentPosition=current.getPosition(); //ignore move
             }
             //Updating player position
-            player.setPosition(currentPosition);
-            System.out.println("Player Positon after dice roll:" + player.getPosition());
+            current.setPosition(currentPosition);
+            System.out.println(current.getName()+" Positon: " + current.getPosition());
+
+            //win check
+            if(current.getPosition()==100)
+                break; //current player wins
+
+            if(!gotLadder){
+                current=(current==player1)?player2:player1;
+            }
         }
-        System.out.println("Congratulations! Player reached Exact Winning 100");
+        //Determining the winner
+        System.out.println("GAME OVER");
+        if(player1.getPosition()==100){
+            System.out.println("Winner: Player 1");
+        }
+        else{
+            System.out.println("Winner: Player 2");
+        }
         System.out.println("Total Dice Rolls: "+diceCount);
     }
 }
