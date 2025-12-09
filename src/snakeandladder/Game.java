ae3fd59ca791ba6cd6fbc8fb9527b0f6c2bc16a0
anmolkr7 <anmolojha952@gmail.com>
2025-12-09 12:27:55 +0530

    UC2-player can perform the dice roll

--- src/snakeandladder/Game.java
+++ src/snakeandladder/Game.java
@@ -3,15 +3,21 @@ package snakeandladder;
 public class Game {
     //single player instance for current game
     private Player player;
+    //Dice roll
+    private Dice dice;
 
     //Initializes the game with a default player
     public Game(){
         player=new Player();
+        dice=new Dice();
     }
 
     //Starts the game
     public void playGame(){
         System.out.println("Welcome to Snake and Ladder Game");
         System.out.println("Player starting position:"+player.getPosition());
+
+        int diceValue=dice.roll();
+        System.out.println("Dice Rolled:"+diceValue);
     }
 }
