fd3eb11c64ad6ba0dd4738cf2e08f3f023a086c2
anmolkr7 <anmolojha952@gmail.com>
2025-12-09 13:09:38 +0530

    UC7: Added player name support

--- src/snakeandladder/Player.java
+++ src/snakeandladder/Player.java
@@ -3,10 +3,11 @@ package snakeandladder;
 public class Player {
     //position- to store player's current location on board
     private int position;
-
+    private String name;
     //Default constructor to initialize the position to 0
-    public Player(){
+    public Player(String name){
         this.position=0;
+        this.name=name;
     }
 
     //getter function to retrieve the private position variable
@@ -18,5 +19,8 @@ public class Player {
     public void setPosition(int position)
     {
         this.position=position;
+    }
+    public String getName(){
+        return name;
     }
 }
